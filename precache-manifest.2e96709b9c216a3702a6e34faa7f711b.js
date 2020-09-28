09873ce41c3ffaf63948b7ae74f4402c6df3f9e5
ArjyaMj <majumderarya6@gmail.com>
2020-09-29 01:49:07 +0530

    Updates

---
+++ precache-manifest.2e96709b9c216a3702a6e34faa7f711b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a048ad8fa9e4f5441ddac847f87d6705",
+    "url": "/bucketlist/index.html"
+  },
+  {
+    "revision": "159d4e4340a8a39bfe09",
+    "url": "/bucketlist/static/css/main.5bd31a2d.chunk.css"
+  },
+  {
+    "revision": "ffd33363b15ed5c05a52",
+    "url": "/bucketlist/static/js/2.d8ae7973.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bucketlist/static/js/2.d8ae7973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "159d4e4340a8a39bfe09",
+    "url": "/bucketlist/static/js/main.0e1b5bb6.chunk.js"
+  },
+  {
+    "revision": "538a601bd11db89ebb81",
+    "url": "/bucketlist/static/js/runtime-main.8aa3d49a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bucketlist/static/media/logo.5d5d9eef.svg"
+  }
+]);
